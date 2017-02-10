537e3e7cb9ed7a886fa4ec06605c4c0adcbe8e83
melvin2016 <melvingeorge10@gmail.com>
2017-02-10 15:42:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   
-    consumer_key:         'IUW8BFH4PpmTNAwici5YVUXxP',
-    consumer_secret:      '3rdkip2ksE7nNpNmSGPlSMA04iNdtdmM11qXLmbMxd7vAjfYL8',
-    access_token:         '829933584437780480-qQvmJBJViNRQESaa04dCB1TVkkp5P5T',
-    access_token_secret:  'qWP0tOapbd327g8tcqMha1P8tBKDq8mnaVa49qZG6y2g7',
+    consumer_key:         '...',
+    consumer_secret:      '....',
+    access_token:         '...',
+    access_token_secret:  '....',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 
 };
